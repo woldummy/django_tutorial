2a01eb87881ffaef9de058623f3a80f82005dc1a
j.wolkenhauer <j.wolkenhauer@schule.bremen.de>
2023-09-13 08:37:49 +0200

    Added file "create_database.py" to create new default database 'bd.sqlite3' if needed i.e. when checkout projekt.

---
+++ mysite/tools/create_database.py
@@ -0,0 +1,35 @@
+# Importieren Sie das Django-Modul
+import os
+from django.conf import settings
+
+DATABASE_NAME = 'db.sqlite3'
+
+DATABASE_NAME = DATABASE_NAME
+
+# Setzen Sie die Einstellungen für das Django-Projekt
+settings.configure(
+    DATABASES={
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(os.getcwd(), '%s' % DATABASE_NAME),
+        }
+    }
+)
+
+# Überprüfen, ob die Datenbank bereits existiert
+if not os.path.exists(DATABASE_NAME):
+    # Wenn die Datenbank nicht existiert, erzeugen Sie sie
+    import django
+    django.setup()
+
+    # Fügen Sie hier Ihre Django-Modelle hinzu, falls erforderlich
+
+    # Erzeugen Sie die leere Datenbank
+    from django.core.management import call_command
+    call_command('migrate')
+
+    print("Leere SQLite-Datenbank: '%s' wurde erfolgreich erstellt." % DATABASE_NAME)
+else:
+    print("Die SQLite-Datenbank '%s' existiert bereits!" % DATABASE_NAME)
+
+
